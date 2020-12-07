ecec20b380d4a9ea4b4e1ad613b02a81278692bb
Gianluigi Silvestre <gsilvest@MBPdiGianluigi.lan>
2020-12-07 20:48:11 +0100

    car and track names mapping

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# -*- coding: utf-8 -*-
 from tkinter import *
 from tkinter import filedialog
 import tkinter.scrolledtext as st 
 import os
 import json
+import io
 import ctypes
 import shutil
 
@@ -28,7 +28,7 @@ class ACCSetupManager(Frame):
         self.lbl3 = Label(self.master, text="No setup opened")
         self.lbl3.place(x=250, y=530, anchor="center")
         
-        OptionList = [
+        self.CarList = [
         "Alpine A110 GT4",
         "AMR V8 Vantage GT3",
         "AMR V12 Vantage GT3",
@@ -57,7 +57,7 @@ class ACCSetupManager(Frame):
         "McLaren 650S GT3",
         "McLaren 720s GT3",
         "Mercedes AMG GT3",
-        "mercedes_amg_gt3_evo",
+        "Mercedes Amg GT3 EVO",
         "Mercedes AMG GT4",
         "Nissan GT R Nismo GT3",
         "Nissan GT-R Nismo GT3",
@@ -68,7 +68,66 @@ class ACCSetupManager(Frame):
         "Reiter Engineering R-EX GT3"
         ]
 
-        OptionList2 = [
+        self.CarListFolder = [
+        "alpine_a110_gt4",
+        "amr_v12_vantage_gt3",
+        "amr_v8_vantage_gt3",
+        "amr_v8_vantage_gt4",
+        "audi_r8_gt4",
+        "audi_r8_lms",
+        "audi_r8_lms_evo",
+        "bentley_continental_gt3_2016",
+        "bentley_continental_gt3_2018",
+        "bmw_m4_gt4",
+        "bmw_m6_gt3",
+        "chevrolet_camaro_gt4r",
+        "ferrari_488_gt3",
+        "ferrari_488_gt3_evo",
+        "ginetta_g55_gt4",
+        "honda_nsx_gt3",
+        "honda_nsx_gt3_evo",
+        "jaguar_g3",
+        "ktm_xbow_gt4",
+        "lamborghini_gallardo_rex",
+        "lamborghini_huracan_gt3",
+        "lamborghini_huracan_gt3_evo",
+        "lamborghini_huracan_st",
+        "lexus_rc_f_gt3",
+        "maserati_mc_gt4",
+        "mclaren_570s_gt4",
+        "mclaren_650s_gt3",
+        "mclaren_720s_gt3",
+        "mercedes_amg_gt3",
+        "mercedes_amg_gt3_evo",
+        "mercedes_amg_gt4",
+        "nissan_gt_r_gt3_2017",
+        "nissan_gt_r_gt3_2018",
+        "porsche_718_cayman_gt4_mr",
+        "porsche_991_gt3_r",
+        "porsche_991ii_gt3_cup",
+        "porsche_991ii_gt3_r"
+        ]
+
+        self.TrackList = [
+        "Barcelona",
+        "Mount Panorama",
+        "Brands Hatch",
+        "Hungaroring",
+        "Imola",
+        "Kyalami",
+        "Laguna Seca",
+        "Misano",
+        "Monza",
+        "Nurburgring",
+        "Paul Ricard",
+        "Silverstone",
+        "Spa",
+        "Suzuka",
+        "Zandvoort",
+        "Zolder"
+        ]
+
+        self.TrackListFolder = [
         "barcelona",
         "mount_panorama",
         "brands_hatch",
@@ -89,14 +148,14 @@ class ACCSetupManager(Frame):
 
         self.variable = StringVar(self)
         self.variable.set("Choose a car")
-        self.opt = OptionMenu(self, self.variable, *OptionList)
-        self.opt.config(width=28, font=('Helvetica', 10))
+        self.opt = OptionMenu(self, self.variable, *self.CarList)
+        self.opt.config(width=28, font=('TkDefaultFont', 10))
         self.opt.grid(row=1, column=0)
 
         self.variable1 = StringVar(self)
         self.variable1.set("Choose a track")
-        self.opt1 = OptionMenu(self, self.variable1, *OptionList2)
-        self.opt1.config(width=28, font=('Helvetica', 10))
+        self.opt1 = OptionMenu(self, self.variable1, *self.TrackList)
+        self.opt1.config(width=28, font=('TkDefaultFont', 10))
         self.opt1.grid(row=1, column=1)
 
         var2 = StringVar()
@@ -125,18 +184,38 @@ class ACCSetupManager(Frame):
             text = self.readFile(fl)
 
     def onLoad(self):
-        for item in os.listdir(os.path.expanduser('~\Documents')+'\Assetto Corsa Competizione\Setups\\' + self.variable.get() + "\\" + self.variable1.get()):
+        for item in os.listdir(os.path.expanduser('~\Documents')+'\Assetto Corsa Competizione\Setups\\' 
+                                                                + self.CarListFolder[self.CarList.index(self.variable.get())] 
+                                                                + "\\" 
+                                                                + self.TrackListFolder[self.TrackList.index(self.variable1.get())]):
+
             self.lb.insert('end', item)
 
     def readFile(self, filename):
-        f = open(filename, "r", encoding='utf-8')
-        text = f.read()
-        data = json.loads(text)
-        self.lbl3.config(text='Loaded setup file for car ' + data['carName'])
-        self.txt.insert('end',text)    
-        
+
+        with io.open(filename, 'r', encoding='utf-8-sig') as json_file:
+            data = json.load(json_file)
+            data = self.flatten_json(self, data)
+            self.lbl3.config(text='Loaded setup file for car ' + data['carName'])
+            #self.txt.insert('end',text)            
         shutil.copyfile(filename, os.path.expanduser('~\Documents')+'\Assetto Corsa Competizione\Setups\\' + self.variable.get() + "\\" + self.variable1.get())
-        return text
+
+    def flatten_json(self, y):
+        out = {}
+
+        def flatten(x, name=''):
+            if type(x) is dict:
+                for a in x:
+                    flatten(x[a], name + a + '_')
+            elif type(x) is list:
+                i = 0
+                for a in x:
+                    flatten(a, name + str(i) + '_')
+                    i += 1
+            else:
+                out[name[:-1]] = x
+        flatten(y)
+        return out
 
 # Main
 if __name__ == "__main__":
